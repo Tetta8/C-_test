9b0e2779b5aefebe7417e251af9eaa06feb2a497
Tetta8 <tester.ta8@gmail.com>
2020-06-24 12:50:52 +0900

    9-2章完了

--- mekai/9_class/MathException.h
+++ mekai/9_class/MathException.h
@@ -1,20 +1,30 @@
 #ifndef ___Math_Exception
 #define ___Math_Exception
 
-class DividedByZero{};
+class MathException {
+public:
+    virtual void display() const {std::cout << "数値演算例外" << std::endl;}
+};
 
-class Overflow{
+class DividedByZero : public MathException{
+public:
+    void display() const{std::cout << "0による除算" << std::endl;}
+};
+
+class Overflow : public MathException{
     int v;
 public:
     Overflow(int val) : v(val){}
     int value() const {return v;}
+    void display() const{std::cout << "オーバーフロー（値は" << v << ")" << std::endl;}
 };
 
-class Underflow{
+class Underflow : public MathException{
     int v;
 public:
     Underflow(int val) : v(val){}
     int value() const{return v;}
+    void display() const{std::cout << "アンダーフロー（値は" << v << ")" << std::endl;}
 };
 
 #endif
---
+++ mekai/9_class/MathExceptionTest3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "MathException.h"
+
+using namespace std;
+
+int check(int value){
+    if(value < 0) throw Underflow(value);
+    if(value > 99) throw Overflow(value);
+    return value;
+}
+
+int add2(int x, int y){
+    return check(x + y);
+}
+
+int sub2(int x, int y){
+    return check(x - y);
+}
+
+int mul2(int x, int y){
+    return check(x * y);
+}
+
+int div2(int x, int y){
+    if(y == 0) throw DividedByZero();
+    return check(x / y);
+}
+
+int main(){
+    int x, y;
+
+    do{cout << "xの値："; cin >> x;} while(x < 0 || x > 99);
+    do{cout << "yの値："; cin >> y;} while(y < 0 || y > 99);
+
+    try{
+        cout << "x + y = " << add2(x, y) << endl;
+        cout << "x + y = " << sub2(x, y) << endl;
+        cout << "x + y = " << mul2(x, y) << endl;
+        cout << "x + y = " << div2(x, y) << endl;
+    }
+    
+
+    catch(const DividedByZero){
+        cout << "DividedByZeroを補足：0による除算が発生！！" << endl;;
+    }
+
+    catch(const MathException&){
+        cout << "MathExceptionを補足：0による除算以外の算術例外が発生！！" << endl;
+    }
+
+    cout << "プログラム終了！！" << endl;
+}
