a758976c72c7fb3c7f4e8510a25bd7e33dcd0298
Tetta8 <tester.ta8@gmail.com>
2020-06-28 10:36:48 +0900

    10-1章完了

--- mekai/10_class/TwinTest.cpp
+++ mekai/10_class/TwinTest.cpp
@@ -4,16 +4,32 @@
 
 using namespace std;
 
+template <>
+inline std::ostream& operator<<(std::ostream& os, const Twin<std::string>& st){
+    return os << "[\"" << st.first() << "\" , \""  << st.second() << "\"]";
+}
+
+template<class Type>
+bool operator<(const Twin<Type>& a, const Twin<Type>& b){
+    if(a.first() < b.first())
+        return true;
+    else if(!(b.first() < a.first()) && (a.second() < b.second()))
+        return true;
+    return false;
+}
+
 int main(){
-    const Twin<int> t1(15, 37);
+    Twin<Twin<int> > t1(Twin<int>(36, 57), Twin<int>(23, 48));
     cout << "t1 = " << t1 << endl;
 
-    Twin<string> t2("ABC", "XYZ");
-    cout << "t2 = " << t2 << endl;
+    Twin<Twin<string> > t2(Twin<string>("ABC", "XYZ"), Twin<string>("ABC", "ZZZ"));
 
+    cout << "t2 = " << t2 << endl;
     cout << "t2の値を変更します。" << endl;
-    cout << "新しい第一値："; cin >> t2.first();
-    cout << "新しい第二値："; cin >> t2.second();
+    cout << "新しい第一値の第一値："; cin >> t2.first().first();
+    cout << "新しい第一値の第二値："; cin >> t2.first().second();
+    cout << "新しい第二値の第一値："; cin >> t2.second().first();
+    cout << "新しい第二値の第二値："; cin >> t2.second().second();
 
     if(!t2.ascending()){
         cout << "第一値＜第二値が成立しませんのでソートします。" << endl;
