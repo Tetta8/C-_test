03ee8ac471904e00bfc7fa1b80e9fcf819630a1a
Tetta8 <tester.ta8@gmail.com>
2020-07-27 13:08:01 +0900

    1-5章完了

---
+++ dokushu/chapter1/list1-14.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    int* ptr = nullptr;
+    *ptr = 42;
+
+    std::cout << "ヌル参照のあと" << std::endl;
+}
---
+++ dokushu/chapter1/list1-15.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int* ptr = nullptr;
+
+    if(ptr == nullptr){
+        std::cout << "ptrはヌルポインターです。" << std::endl;
+    }else{
+        *ptr = 42;
+    }
+}
