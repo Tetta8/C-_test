be1f1a2af4b03921539ca7a83b8382e8ee73b5d5
Tetta8 <tester.ta8@gmail.com>
2020-08-09 15:53:03 +0900

    cmakeの手順確認

---
+++ cmaketest/sample1/build/CMakeFiles/sample.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample.dir/main.cpp.o"
+  "CMakeFiles/sample.dir/hello.cpp.o"
+  "sample.pdb"
+  "sample.exe"
+  "libsample.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmaketest/sample2/build/CMakeFiles/greetings.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/cygwin64/home/copen/CPP_test/cmaketest/sample2/good_morning.cpp" "/cygdrive/c/cygwin64/home/copen/CPP_test/cmaketest/sample2/build/CMakeFiles/greetings.dir/good_morning.cpp.o"
+  "/cygdrive/c/cygwin64/home/copen/CPP_test/cmaketest/sample2/hello.cpp" "/cygdrive/c/cygwin64/home/copen/CPP_test/cmaketest/sample2/build/CMakeFiles/greetings.dir/hello.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "greetings_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
