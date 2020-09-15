a402364365465c96bee5e9604e3c10bb85d37d80
Tetta8 <tester.ta8@gmail.com>
2020-09-15 13:16:38 +0900

    8-1,8-2章完了

---
+++ dokushu/chapter8/list8-1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(){
+    std::cout << 123.456f << std::endl;
+
+    std::cout << 123456 << std::endl;
+
+    std::cout.setf(std::ios::scientific);
+
+    std::cout << 123.456f << std::endl;
+
+    std::cout << 123456 << std::endl;
+
+    std::cout.setf(std::ios::hex);
+
+    std::cout << 123.456f << std::endl;
+
+    std::cout << 123456 << std::endl;
+
+    std::cout.unsetf(std::ios::dec);
+
+    std::cout << 123456 << std::endl;
+}
