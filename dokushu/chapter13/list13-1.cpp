25caadddeb927160f054b3b066093f475ceb9502
Tetta8 <tester.ta8@gmail.com>
2020-09-16 13:16:15 +0900

    13-1章完了

---
+++ dokushu/chapter13/list13-1.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(){
+    const char* strA = "Hello, ";
+    const char* strB = "world.";
+
+    char* strC = (char*)malloc(strlen(strA) + strlen(strB) + 1);
+
+    strcpy(strC, strA);
+    strcat(strC, strB);
+    puts(strC);
+
+    if(strcmp(strC, "Hello, world.") == 0){
+        puts("oks");
+    }
+    free(strC);
+}
---
+++ dokushu/chapter13/list13-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string strA = "Hello, ";
+    std::string strB = "world.";
+
+    std::string strC = strA + strB;
+    std::cout << strC << std::endl;
+
+    if(strC == "Hello, world."){
+        std::cout << "ok." << std::endl;
+    }
+}
